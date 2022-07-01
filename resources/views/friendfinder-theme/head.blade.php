d5686efaaaa05bbda5856d50f91fc49cd85707a4
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-07-01 23:15:50 +0500

    Update

---
+++ resources/views/friendfinder-theme/head.blade.php
@@ -0,0 +1,11 @@
+<!-- Stylesheets
+================================================= -->
+<link rel="stylesheet" href="{{asset('assets/FriendFinder-Theme/css/bootstrap.min.css')}}" />    
+<link rel="stylesheet" href="{{asset('assets/FriendFinder-Theme/css/style.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/FriendFinder-Theme/css/ionicons.min.css')}}" />
+<link rel="stylesheet" href="{{asset('assets/FriendFinder-Theme/css/font-awesome.min.css')}}" />
+<!--Google Font-->
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i" rel="stylesheet">
+<!--Favicon-->
+<link rel="shortcut icon" type="image/png" href="{{asset('assets/FriendFinder-Theme/images/fav.png')}}"/>
+@yield('pageStyles')
