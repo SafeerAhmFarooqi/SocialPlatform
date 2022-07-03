ef8b068f60695048973f130207402b3cf58124b3
SafeerAhmFarooqi <saf_farooqi@hotmail.com>
2022-07-03 12:49:33 +0500

    Update

---
+++ app/Mail/UserAccountDeactivationEmail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserAccountDeactivationEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        //return $this->view('view.name');
+        return $this->subject('Shop Account Deactivation Alert From asadsohail.com')
+                    ->view('email.user-account-deactivation-alert');
+    }
+}
